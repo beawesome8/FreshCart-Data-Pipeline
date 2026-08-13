5f5dafb64f0dcd849a2d98f9a60210bceed9be06
benjamin emmanuel <benemmanuel80@gmail.com>
2026-08-14 00:33:48 +0200

    Retrigger CI/CD after connection fix

--- sql/06_reporting_views.sql
+++ sql/06_reporting_views.sql
@@ -68,3 +68,4 @@ SELECT * FROM consumption_sch.vw_monthly_revenue;
 SELECT * FROM consumption_sch.vw_delivery_performance ORDER BY delivered_rate_pct DESC;
 SELECT * FROM common.vw_latest_dq_status;-- CI/CD trigger test
 -- retest after chmod fix
+-- retest after connection flag fix
