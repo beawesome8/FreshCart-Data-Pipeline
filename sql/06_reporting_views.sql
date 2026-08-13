a4a48a7fe550c3ac8aa916e4ff031326206b15ac
benjamin emmanuel <benemmanuel80@gmail.com>
2026-08-14 00:25:36 +0200

    Trigger CI/CD test run (real content change)

--- sql/06_reporting_views.sql
+++ sql/06_reporting_views.sql
@@ -66,4 +66,4 @@ ORDER BY 1 DESC;
 -- ============================================================
 SELECT * FROM consumption_sch.vw_monthly_revenue;
 SELECT * FROM consumption_sch.vw_delivery_performance ORDER BY delivered_rate_pct DESC;
-SELECT * FROM common.vw_latest_dq_status;
+SELECT * FROM common.vw_latest_dq_status;-- CI/CD trigger test
